4e5acfe5095f0bbf9f1d9aa6dd86149b2e79c44d
Jamie Mair <JamieMair@users.noreply.github.com>
2022-09-29 19:41:22 +0000

    Added array comparison results

--- blog/main.jl
+++ blog/main.jl
@@ -23,6 +23,22 @@ function simple_monte_carlo_threaded(n, T)
 	return x
 end
 
+function random_walk!(x, T)
+	fill!(x, zero(eltype(x))) # Set all elements of the x array to zero
+	cache = similar(x) # Create a cache array of the same size and type as x
+	for t in 1:T
+		Random.randn!(cache) # Populate the cached memory with random numbers
+		x .+= cache
+	end
+	return x
+end
+function simple_monte_carlo_array(n, T)
+	x = zeros(n)
+	random_walk!(x, T)
+	return x
+end
+
+
 function read_cpp_csv(filename = "cpp_results.csv")
     df = DataFrame(CSV.File(filename))
     rename!(df, :time_ns=>:times_cpp_ns)
@@ -51,6 +67,18 @@ function add_threaded_benchmark!(df)
 
     julia_times = bench_julia.(Iterators.zip(df.n, df.T)) .* 1e9;
     df[:, :times_jl_threaded_ns] = julia_times;
+    nothing
+end
+function add_array_benchmark!(df)
+    function bench_julia(args)
+        n, T = args
+        time = @belapsed simple_monte_carlo_array($n, $T);
+        GC.gc();
+        return time;
+    end
+
+    julia_times = bench_julia.(Iterators.zip(df.n, df.T)) .* 1e9;
+    df[:, :times_jl_array_ns] = julia_times;
     nothing
 end
 
@@ -88,5 +116,12 @@ function create_threaded_plot(df; num_threads=Threads.nthreads())
     plt = plot(df.n, df.n ./ df.n .* num_threads, label="# Threads", linestyle=:dash, lw=2)
     plt = compare_benches(df, :times_cpp_ns=>"CPP", :times_jl_threaded_ns=>"Threaded"; legend=:topleft, new_plot=false)
     savefig("figures/monte_carlo_threaded.png")
+    return plt
+end
+
+function create_array_plot(df; num_threads=Threads.nthreads())
+    plt = plot(df.n, df.n ./ df.n .* num_threads, label="# Threads", linestyle=:dash, lw=2)
+    plt = compare_benches(df, :times_cpp_ns=>"CPP", :times_jl_threaded_ns=>"Threaded", :times_jl_array_ns=>"Array"; legend=:topleft, new_plot=false)
+    savefig("figures/monte_carlo_array.png")
     return plt
 end
