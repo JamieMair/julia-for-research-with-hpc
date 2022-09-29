a8773d9fcec0a7a5f9ddcb940fba8c651ee1b18b
Jamie Mair <JamieMair@users.noreply.github.com>
2022-09-29 16:05:09 +0000

    Started working on the blog code

---
+++ blog/main.jl
@@ -0,0 +1,47 @@
+using Random
+using BenchmarkTools
+using Plots
+using DataFrames
+using CSV
+
+function simple_monte_carlo(n, T)
+	x = zeros(n)	
+	for i in eachindex(x)
+		for t in 1:T	
+			x[i] += Random.randn()	
+		end	
+	end
+	return x
+end
+
+function read_cpp_csv(filename = "cpp_results.csv")
+    df = DataFrame(CSV.File(filename))
+    rename!(df, :time_ns=>:times_cpp_ns)
+    return df;
+end
+
+function add_benchmarks!(df)
+    function bench_julia(args)
+        n, T = args
+        time = @belapsed simple_monte_carlo($n, $T);
+        GC.gc();
+        return time;
+    end
+
+    julia_times = bench_julia.(Iterators.zip(df.n, df.T)) .* 1e9;
+    df[:, :times_jl_ns] = julia_times;
+    nothing
+end
+
+function save_bench(df, filename="julia_vs_cpp_results.csv")
+    CSV.write(filename, df);
+end
+
+function compare_benches(df)
+    plt = plot(df.n, df.times_cpp_ns./df.times_jl_ns, label="C++", markershape=:diamond, lw=2)
+    plot!(plt, df.n, df.times_jl_ns./df.times_jl_ns, label="Julia", markershape=:circle, lw=2)
+    plot!(plt; xscale=:log10, legend=:topleft)
+    ylabel!(plt, "Relative Time")
+    xlabel!(plt, "n")
+    return plt
+end
---
+++ blog/monte_carlo.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+#include <chrono>
+#include <fstream>
+
+using namespace std::chrono;
+
+double *random_walk(int n, int T, std::normal_distribution<double> randn, std::mt19937_64 rng)
+{
+    double *x = new double[n];
+    for (int i = 0; i < n; i++)
+    {
+        double x_t = 0.0;
+        for (int t = 0; t < T; t++)
+        {
+            x_t += randn(rng);
+        }
+        x[i] = x_t;
+    }
+    return x;
+}
+
+double measure_random_walk_time(int n, int T, int repeats)
+{
+    std::random_device rd{};
+    std::mt19937_64 rng{rd()};
+    std::normal_distribution<> randn{0, 1};
+    double min_time = MAXFLOAT;
+
+    for (size_t r = 0; r < repeats; r++)
+    {
+        auto start = high_resolution_clock::now();
+        double *x = random_walk(n, T, randn, rng);
+        auto stop = high_resolution_clock::now();
+        double duration = (double)duration_cast<nanoseconds>(stop - start).count();
+        if (duration < min_time)
+        {
+            min_time = duration;
+        }
+        delete[] x; // don't leak memory
+    }
+    return min_time;
+}
+
+int main()
+{
+    std::random_device rd{};
+    std::mt19937_64 rng{rd()};
+
+    // values near the mean are the most likely
+    // standard deviation affects the dispersion of generated values from the mean
+    std::normal_distribution<> randn{0, 1};
+
+    int repeats = 500;
+    int ns[] {
+        8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384
+    };
+    int T = 100;
+    std::string filename("cpp_results.csv");
+    std::fstream csv_file;
+
+    csv_file.open(filename, std::ios_base::out);
+
+    csv_file << "n,T,time_ns" << std::endl;
+
+    for (int n:ns)
+    {
+        double min_time = measure_random_walk_time(n, T, repeats);
+        csv_file << n << "," << T << "," << min_time << std::endl;
+    }    
+
+    csv_file.close();
+}
